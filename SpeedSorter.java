48eb8804674eeb9d5f004c0db2da0cd23a9c70b7
t-wang01 <t.wang01@yahoo.com>
2017-05-23 10:41:32 -0400

    Changed to a JFrame

--- SpeedSorter.java
+++ SpeedSorter.java
@@ -4,19 +4,29 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
-public class SpeedSorter{
+public class SpeedSorter extends JFrame{
 	private SpeedSorterMenuPanel menu;
 	private SpeedSorterGamePanel game;
 	private SpeedSorterControlPanel control;
 	private SpeedSorterMenuBar menuBar;
-	private JFrame frame;
 	private JPanel panel;
 	
 	public SpeedSorter(){
+		super("SpeedSorter v1");
+		setSize(500, 400);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		
 		menu = new SpeedSorterMenuPanel(this);
 		game = new SpeedSorterGamePanel(this);
 		control = new SpeedSorterControlPanel(game);
 		menuBar = new SpeedSorterMenuBar(control);
+		
+		panel = new JPanel(new BorderLayout());
+		panel.add(menu, BorderLayout.CENTER);
+		
+		setJMenuBar(menuBar);
+		add(panel);
 	}
 	
 	public static void main(String[] args){
@@ -26,32 +36,20 @@ public class SpeedSorter{
 	    catch(Exception ex){};
 	    
 		SpeedSorter game = new SpeedSorter();
-	    
-		game.frame = new JFrame("SpeedSorter v1");
-		game.frame.setSize(500, 400);
-		game.frame.setLocationRelativeTo(null);
-		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
-		
-		game.panel = new JPanel(new BorderLayout());
-		
-		game.panel.add(game.menu, BorderLayout.CENTER);
-		
-		game.frame.setJMenuBar(game.menuBar);
-		game.frame.add(game.panel);
-		game.frame.setVisible(true);
+		game.setVisible(true);
 	}
 	
 	public void menuToGame(){
-		frame.remove(panel);
+		remove(panel);
 		
 		panel.remove(menu);
 		panel.add(game, BorderLayout.CENTER);
 		panel.add(control, BorderLayout.LINE_END);
 		
-		frame.add(panel);
+		add(panel);
 		
-		frame.revalidate();
-		frame.repaint();
+		revalidate();
+		repaint();
 	}
 	
 	public boolean stopwatchIsRunning(){
